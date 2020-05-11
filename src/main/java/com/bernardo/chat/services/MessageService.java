c265dcd9cbfe0dd6dd6c3677bea719f029cb8c02
Bernardo Sobral Werneck <bernardo.s.werneck@gmail.com>
2020-05-11 00:53:00 -0300

    Chat

--- src/main/java/com/bernardo/chat/services/MessageService.java
+++ src/main/java/com/bernardo/chat/services/MessageService.java
@@ -9,21 +9,26 @@ import org.springframework.stereotype.Service;
 import com.bernardo.chat.domain.Message;
 import com.bernardo.chat.dto.MessageCommand;
 import com.bernardo.chat.repositories.MessageRepository;
+import com.bernardo.chat.repositories.RoomRepository;
+import com.bernardo.chat.repositories.UserRepository;
 
 @Service
 public class MessageService {
     private MessageRepository messageRepository;
     private SimpMessagingTemplate simpMessagingTemplate;
+    private RoomRepository roomRepository;
+    private UserRepository userRepository;
+//    private SessionBean sessionBean;
 
     @Autowired
     public MessageService(MessageRepository messageRepository,
-    					  SimpMessagingTemplate simpMessagingTemplate) {
+    					  SimpMessagingTemplate simpMessagingTemplate,
+    					  RoomRepository roomRepository,
+    					  UserRepository userRepository) {
         this.messageRepository = messageRepository;
         this.simpMessagingTemplate = simpMessagingTemplate;
-    }
-
-    public void save(Message message) {
-        this.messageRepository.save(message);
+        this.roomRepository = roomRepository;
+        this.userRepository = userRepository;
     }
 
     public List<Message> findAllByRoomIdOrderByCreatedDate(Integer roomId) {
@@ -31,8 +36,15 @@ public class MessageService {
     }
     
     public void receiveMessage(MessageCommand message) {
-    	
-    	System.out.println(message.getContent());
-    	this.simpMessagingTemplate.convertAndSend("/topic/message", message);
+    	messageRepository.save(convert(message));
+    	this.simpMessagingTemplate.convertAndSend("/topic/" + message.getRoomId().toString(), message);
     }
+
+	private Message convert(MessageCommand message) {
+		Message newMessage = new Message();
+		newMessage.setText(message.getContent());
+		newMessage.setRoom(roomRepository.findById(message.getRoomId()).get());
+		newMessage.setUser(userRepository.findById(message.getUserId()).get());
+		return newMessage;
+	}
 }
