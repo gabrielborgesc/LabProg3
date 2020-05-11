4aabc3ccfcc1c8c4f03e755edd70fc0e20a003d2
plenamente <gustavoplenamente@gmail.com>
2020-05-11 09:45:51 -0300

    adicionar logout ao front

--- src/main/resources/static/chat/chatSelector.js
+++ src/main/resources/static/chat/chatSelector.js
@@ -71,3 +71,27 @@ function loadChat()
 }
 
 $("document").ready(retrieveChat);
+
+function logout()
+{
+	fetch("/logout",
+		{
+			method: 'POST',
+			body: JSON.stringify({}),
+			headers: {
+				'Content-Type': 'application/json',
+				'access-control-allow-origin': '*'
+			}
+		})
+		.then(response => response.text())
+		.then(data => success = data)
+		.then(logoutCallback);
+}
+
+function logoutCallback()
+{
+	if(success === "true")
+		document.location = 'index.html';
+	else
+		window.alert("Não foi possível realizar essa operação");
+}
